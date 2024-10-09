d8ff8e13e76b3ef60908b75d6b0c39600a13fe3d
Hongzhen Wang <hw6u19@soton.ac.uk>
2024-10-09 15:05:45 +0800

    fix the stability

--- opengait/modeling/models/__init__.py
+++ opengait/modeling/models/__init__.py
@@ -6,12 +6,14 @@ from importlib import import_module
 # iterate through the modules in the current package
 package_dir = Path(__file__).resolve().parent
 for (_, module_name, _) in iter_modules([str(package_dir)]):
+    try:
+        # Import the module and iterate through its attributes
+        module = import_module(f"{__name__}.{module_name}")
+        for attribute_name in dir(module):
+            attribute = getattr(module, attribute_name)
+            if isclass(attribute):
+                # Add the class to this package's variables
+                globals()[attribute_name] = attribute
 
-    # import the module and iterate through its attributes
-    module = import_module(f"{__name__}.{module_name}")
-    for attribute_name in dir(module):
-        attribute = getattr(module, attribute_name)
-
-        if isclass(attribute):
-            # Add the class to this package's variables
-            globals()[attribute_name] = attribute
+    except Exception as e:
+        print(f"Failed to import {module_name}: {e}")
---
+++ opengait/package_test.py
@@ -0,0 +1,3 @@
+import modeling.models
+
+pass
